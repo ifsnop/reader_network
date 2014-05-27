c45ac551e2cf0d2ae8710e754cb68cf53c648c23
Diego Torres <diego.torres@gmail.com>
2014-05-27 09:25:55 +0200

    added sparc precompiled binaries

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 rm distrib/rn066.tar.bz2
 tar cvfj distrib/rn066.tar.bz2 \
-    bin/client bin/client_time \
+    bin/client* bin/client_time* \
     bin/reader_network* \
-    bin/reader_rrd3* bin/cleanast bin/scripts/* bin/conf/*
+    bin/reader_rrd3* bin/cleanast* bin/scripts/* bin/conf/*
