fea883b14e9842c2592cc7b335ed46d65f8488f9
Diego Torres <diego.torres@gmail.com>
2014-06-17 15:27:34 +0000

    bumping to 0.67

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
-rm distrib/rn066.tar.bz2
-tar cvfj distrib/rn066.tar.bz2 \
+rm distrib/rn067.tar.bz2
+tar cvfj distrib/rn067.tar.bz2 \
     bin/client* bin/client_time* \
     bin/reader_network* \
     bin/reader_rrd3* bin/cleanast* bin/scripts/* bin/conf/*
