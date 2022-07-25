64f8c596ef904a4e224ad1f246e5fa87c6afc9b0
Diego Torres <diego.torres@gmail.com>
2022-07-25 01:31:31 +0000

    support 0.79 in build script

--- package.sh
+++ package.sh
@@ -1,9 +1,9 @@
-rm distrib/rn078.tar.bz2
-tar cvfj distrib/rn078.tar.bz2 \
+rm distrib/rn079.tar.bz2
+tar cvfj distrib/rn079.tar.bz2 \
     bin/client* \
     bin/reader_network* \
     bin/reader_rrd3* bin/cleanast* bin/scripts/* bin/conf/* bin/filter*
-cp distrib/rn078.tar.bz2 /home/eval
-scp distrib/rn078.tar.bz2 eval@nemo:.
-scp distrib/rn078.tar.bz2 eval@carol:.
-scp distrib/rn078.tar.bz2 eval@coral:.
+cp distrib/rn079.tar.bz2 /home/eval
+scp distrib/rn079.tar.bz2 eval@nemo_192:.
+scp distrib/rn079.tar.bz2 eval@carol_192:.
+scp distrib/rn079.tar.bz2 eval@coral_192:.
